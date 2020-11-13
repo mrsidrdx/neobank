a124760509dd45d7dd1744e97c8c0f9e744a7d3f
Siddhartha Satyakama <52314451+mrsidrdx@users.noreply.github.com>
2020-11-13 14:32:33 +0530

    all fronted pages added

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import index
+from .views import index, about, blog, news_event, manageexpenses, contact, wealth, services
+
+app_name = 'frontend'
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('blog/', blog, name='blog'),
+    path('news-event/', news_event, name='news_event'),
+    path('services/', services, name='services'),
+    path('contact/', contact, name='contact'),
+    path('services/employee/manageexpenses/', manageexpenses, name='services_manageexpenses'),
+    path('services/employee/wealth/', wealth, name='services_wealth'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+def about(request, *args, **kwargs):
+    return render(request, 'frontend/aboutus.html')
+
+def blog(request, *args, **kwargs):
+    return render(request, 'frontend/blog.html')
+
+def contact(request, *args, **kwargs):
+    return render(request, 'frontend/contact-us.html')
+
+def news_event(request, *args, **kwargs):
+    return render(request, 'frontend/news-event.html')
+
+def services(request, *args, **kwargs):
+    return render(request, 'frontend/services.html')
+
+def manageexpenses(request, *args, **kwargs):
+    return render(request, 'frontend/manage-expenses.html')
+
+def wealth(request, *args, **kwargs):
+    return render(request, 'frontend/wealth.html')
